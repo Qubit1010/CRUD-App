533182ea3c17e6addd46dd2b653ae1409567dacb
Qubit1010 <hassanaleem86@gmail.com>
2023-03-27 03:35:49 +0500

    CRUD App Initialized

---
+++ backend/db.js
@@ -0,0 +1,35 @@
+const mongodb = require("mongodb");
+
+const MongoClient = mongodb.MongoClient;
+const mongoDbUrl =
+  "mongodb+srv://Aleem:icRJf6BwQ5oPUKJO@cluster-crud.fs5csha.mongodb.net/?retryWrites=true&w=majority";
+
+let _db;
+
+const initDb = (callback) => {
+  if (_db) {
+    console.log("Database is already initialized!");
+    return callback(null, _db);
+  }
+  MongoClient.connect(mongoDbUrl)
+    .then((client) => {
+      _db = client.db;
+      callback(null, _db);
+    })
+    .catch((err) => {
+      callback(err);
+    });
+};
+
+const getDb = () => {
+  if (!_db) {
+    throw Error("Database not intialized");
+  }
+
+  return _db;
+};
+
+module.exports = {
+  initDb,
+  getDb,
+};
